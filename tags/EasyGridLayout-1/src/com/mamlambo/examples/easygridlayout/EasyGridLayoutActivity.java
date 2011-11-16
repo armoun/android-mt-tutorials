7c108255832f23fa304170ef64e45abe77b5eaed
kf6nvr@gmail.com <kf6nvr@gmail.com@435380ad-7229-49bd-0862-fbbe30098be8>
2011-11-16 01:20:02 +0000

    tag for initial version

---
+++ tags/EasyGridLayout-1/src/com/mamlambo/examples/easygridlayout/EasyGridLayoutActivity.java
@@ -0,0 +1,13 @@
+package com.mamlambo.examples.easygridlayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EasyGridLayoutActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.number_pad);
+    }
+}
