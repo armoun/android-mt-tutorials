f897a0afde8e28de8e7e0746143fb6bba7ddb318
kf6nvr <kf6nvr@gmail.com>
2011-11-22 18:57:43 +0000

    Updates.

--- EasyGridLayout/src/com/mamlambo/examples/easygridlayout/EasyGridLayoutActivity.java
+++ EasyGridLayout/src/com/mamlambo/examples/easygridlayout/EasyGridLayoutActivity.java
@@ -8,6 +8,16 @@ public class EasyGridLayoutActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.number_pad);
+        // a colorful grid layout, for fun
+        //setContentView(R.layout.main);
+        
+        // This layout starts with the 1 button and specially places the / button
+        //setContentView(R.layout.number_pad);
+        
+        // This layout starts with the / button, no extra special placement needed
+        setContentView(R.layout.number_pad2);
+        
+        // this has no column or row spans, no special placements
+        //setContentView(R.layout.simple_numbers);
     }
 }
