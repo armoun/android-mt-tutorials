586ec99c4cce5ee17329f640f1266c9d5c8b99b9
kf6nvr <kf6nvr@gmail.com>
2012-02-07 14:07:50 +0000

    Now a live wallpaper.

--- FallingSnow/src/com/mamlambo/fallingsnow/FallingSnowActivity.java
+++ FallingSnow/src/com/mamlambo/fallingsnow/FallingSnowActivity.java
@@ -2,15 +2,21 @@ package com.mamlambo.fallingsnow;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class FallingSnowActivity extends Activity {
 	private FallingSnowView snowView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.preview);
         snowView = new FallingSnowView(this);
-        setContentView(snowView);
+        FrameLayout frame = (FrameLayout) findViewById(R.id.snowPreview);
+        frame.addView(snowView);
+
     }
     
     @Override
@@ -23,5 +29,10 @@ public class FallingSnowActivity extends Activity {
     protected void onPause() {
         super.onPause();
         snowView.pause();
+    }
+    
+    public void onWallpaperSettings(View view) {
+        Intent wallpaperSettings = new Intent(Intent.ACTION_SET_WALLPAPER);
+        startActivity(wallpaperSettings);
     }
 }
